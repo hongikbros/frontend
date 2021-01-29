1183231838843d753f95e23b726b2c57f4bd14eb
jooam <ja960508@naver.com>
2021-01-30 01:10:53 +0900

    main header 레이아웃 구성 header 내부에 logo, navbar, login button이 위치하는 형태 index.css에 html 시맨틱 태그  스타일링 unset

---
+++ src/component/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from '../css/navbar.module.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className={style.navbar}>
+        <ul>gkdl</ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
