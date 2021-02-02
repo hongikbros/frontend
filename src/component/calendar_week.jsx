3abdaa7d9be4fbb5b82d5f7d51ad767eab5be52c
jooam <ja960508@naver.com>
2021-02-02 13:57:25 +0900

    chore: props validation error 예외처리 - package,json에서 eslint rule 수정

--- src/component/calendar_week.jsx
+++ src/component/calendar_week.jsx
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import style from '../css/calendar.module.css';
 
-class CalendarWeek extends Component {
-  render() {
-    // const getDay = () =>
-    //   Array(7)
-    //     .fill(0)
-    //     .map((data, index) => {
-    //       let day = this.props.today
-    //         .clone()
-    //         .startOf('year')
-    //         .week(this.props.week)
-    //         .startOf('week')
-    //         .add(index, 'day');
-    //     });
-    return <div className={style.calendarWeek}>하이</div>;
-  }
-}
+const CalendarWeek = (props) => {
+  const a = 324;
+  const { week } = props;
+  return (
+    <div className={style.calendarWeek}>
+      하이{a}
+      {week}
+    </div>
+  );
+};
 
 export default CalendarWeek;
