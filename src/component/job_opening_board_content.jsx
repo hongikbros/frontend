075fdc7f7cf582323736123130cd94410e036e58
jooam <ja960508@naver.com>
2021-02-05 16:08:37 +0900

    feat: styling for distinction - icons is not contained in list yet - for testing little bit of styling is added

--- src/component/job_opening_board_content.jsx
+++ src/component/job_opening_board_content.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import style from '../css/job_opening_board.module.css';
 
-const JobOpeningBoardContent = () => <div>헬로</div>;
-
+const JobOpeningBoardContent = (props) => {
+  const { companyName, deadline, title } = props;
+  return (
+    <div className={style.jobOpeningBoardContent}>
+      {companyName}
+      <br />
+      {deadline}
+      <br />
+      {title}
+      <br />
+    </div>
+  );
+};
 export default JobOpeningBoardContent;
