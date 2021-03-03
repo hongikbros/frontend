67b0d3a0a41d376199d69999d46070e7fa9db11a
jooam <ja960508@naver.com>
2021-03-03 22:58:44 +0900

    ready to refactoring

---
+++ src/component/header/header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = ({ loginState }) => <h1>{loginState}다</h1>;
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './component/header';
-import Main from './component/main';
+import App from './app';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
-    <Main />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
